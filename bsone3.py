af8a1885f7943a1029b0e5252766fe05781172af
akashsingh8998 <48746941+akashsingh8998@users.noreply.github.com>
2019-03-20 14:51:25 +0530

    Update bsone3.py

--- bsone3.py
+++ bsone3.py
@@ -1,6 +1,6 @@
 # your code goes here
 ch = input()
-if (ch>"a" and ch<"z") or (ch>"A" and ch<"Z"):
+if (ch>="a" and ch<="z") or (ch>="A" and ch<="Z"):
 	if ch in ('a','A','e','E','i','I','o','O','u','U'):
 		print("Vowel")
 	else:
