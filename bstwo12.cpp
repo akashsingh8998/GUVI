3094359c26d28c59e99c62e1d8f54739bdb9dcfd
akashsingh8998 <48746941+akashsingh8998@users.noreply.github.com>
2019-03-22 18:28:30 +0530

    Create bstwo12.cpp

---
+++ bstwo12.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+	// your code goes here
+	int N,temp,rev,val;
+	cin>>N;
+	temp = N;
+	rev = 0;
+	if(N<=1000)
+	{
+	while(N>0)
+	{
+		val = N % 10;
+		rev = rev * 10 + val;
+		N = N / 10;
+	}
+	if(temp==rev)
+	{
+		cout<<"yes";
+	}
+	else
+	{
+		cout<<"no";
+	}
+	}
+	return 0;
+}
