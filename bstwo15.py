96edefc4fe0fc5ea3d0f0f3b4d6dce4dbe718a67
akashsingh8998 <48746941+akashsingh8998@users.noreply.github.com>
2019-03-23 13:12:07 +0530

    Update bstwo15.py

--- bstwo15.py
+++ bstwo15.py
@@ -2,9 +2,14 @@
 N,Q = input().split()
 N = int(N)
 Q = int(Q)
-for i in range(N+1,Q-1):
-	if(i%2==0):
-		print(i,end=" ")
-for i in range(Q-1,Q):
-	if(i%2==0):
-		print(i,end="")
+if(N+4 == Q):
+	for i in range(N+1,Q):
+		if(i%2==0):
+			print(i,end="")
+else:
+	for i in range(N+1,Q-1):
+		if(i%2==0):
+			print(i,end=" ")
+	for i in range(Q-1,Q):
+		if(i%2==0):
+			print(i,end="")
