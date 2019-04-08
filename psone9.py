0a9468cebb7579fa164e0ef2c103cdc382791963
akashsingh8998 <48746941+akashsingh8998@users.noreply.github.com>
2019-04-08 14:30:00 +0530

    Create psone9.py

---
+++ psone9.py
@@ -0,0 +1,15 @@
+# your code goes here
+n,k = input().split()
+n = int(n)
+k = int(k)
+a = []
+for j in range (n,k+1):
+	flag = 1
+	for i in range(2,j):
+		if(j%i==0):
+			flag = 0
+			break
+	if(flag==1):
+		a.append(str(j))
+#print(" ".join(a))
+print(len(a))
