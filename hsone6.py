8fddc954138a791ab10b287327029d2560b913de
akashsingh8998 <48746941+akashsingh8998@users.noreply.github.com>
2019-04-09 14:23:37 +0530

    Create hsone6.py

---
+++ hsone6.py
@@ -0,0 +1,29 @@
+# your code goes here
+import collections
+
+n = int(input())
+a = []
+b = input().split()
+for i in range(0,n):
+	a.append(b[i])
+
+results = collections.Counter(a)
+c = []
+#print(results)
+for i in results:
+	if(results[i]>1):
+		c.append(i)
+		break
+#c = sorted(c)
+if not c:
+	print("unique")
+else:
+	#print(" ".join(c[0]))
+	#print(c)
+	seen = set()
+	for i in a:
+		if i in seen:
+			print(i)
+			break
+		else:
+			seen.add(i)
