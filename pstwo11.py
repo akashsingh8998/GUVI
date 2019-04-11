6f470259c377374294e8115c0dc4cca123b10377
akashsingh8998 <48746941+akashsingh8998@users.noreply.github.com>
2019-04-12 00:24:57 +0530

    Create pstwo11.py

---
+++ pstwo11.py
@@ -0,0 +1,8 @@
+# your code goes here
+a = ["Monday","Tuesday","Wednesday","Thursday","Friday"]
+b = ["Saturday","Sunday"]
+n = input().title()
+if n in b:
+	print("yes")
+elif n in a:
+	print("no")
