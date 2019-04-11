7e726aed2f54b404ebb89d19f5eb6b6cf3cfc566
akashsingh8998 <48746941+akashsingh8998@users.noreply.github.com>
2019-04-11 11:53:56 +0530

    Update hsone7.py

--- hsone7.py
+++ hsone7.py
@@ -1,16 +1,14 @@
 # your code goes here
-n = int(input())
-a = []
-b = input().split()
+n=int(input())
+a = [int(y) for y in input().split()]
 for i in range(0,n):
-	a.append(int(b[i]))
-
-d = []
-
-for i in range(0,n):
-	if (i%2==0) and (a[i]%2==1):
-		d.append(str(a[i]))
-	elif (i%2==1) and (a[i]%2==0):
-		d.append(str(a[i]))
-print(" ".join(d))
-		
+    if i<n-1:
+        b=' '
+    else:
+        b=''
+    if i%2==0:
+        if a[i]%2!=0:
+            print(a[i],end=b)
+    elif i%2!=0:
+        if a[i]%2==0:
+            print(a[i],end=b)
