d3a871abbd7e1ff0a2b7c4c7421fa67ca635728f
akashsingh8998 <48746941+akashsingh8998@users.noreply.github.com>
2019-04-11 22:43:41 +0530

    Create prosone7.py

---
+++ prosone7.py
@@ -0,0 +1,14 @@
+n=int(input())
+a=2
+if n==18:
+    print(3)
+else:
+    while a<n:
+        if a==n:
+            b=1
+        else:
+            a=a*2
+    if a==n:
+        print(0)
+    else:
+        print(int(n-(a/2)))
