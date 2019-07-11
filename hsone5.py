312c5ccf5c0a209089d690d18184bd22b765763b
akashsingh8998 <48746941+akashsingh8998@users.noreply.github.com>
2019-07-11 16:46:44 +0530

    Create hsone5.py

---
+++ hsone5.py
@@ -0,0 +1,13 @@
+# your code goes here
+st=list(map(str,input()))
+v=t=0
+for i in range(0,len(st)-1):
+  q=st[i]
+  if int(q)!=0:
+   for j in range(i+1,i+2):
+    q=q+st[j]
+    if int(q)<27 and int(q)>0: v=v+1
+    elif int(q)==0: v=v-1
+    else: break
+if v!=1: t=v%2
+print(v+t+1)
